96193353c2a341f88c7ca454b77f6a0ad8903a8a
Rheyhan <rheyhanfahry@apps.ipb.ac.id>
2023-12-31 16:05:12 +0700

    Wkwkwkwk

--- 7th week/test.py
+++ 7th week/test.py
@@ -1,17 +1,18 @@
-import sys
 import os
+import sys
+import pandas as pd
+import numpy as np
 
-if (counter:=len(sys.argv)) ==1:
+if (temp:= len(sys.argv)) <= 2:
     sys.exit("Too few command-line arguments")
-elif counter>2:
+elif temp >3:
     sys.exit("Too many command-line arguments")
-
 path=sys.argv[1]
-if not os.path.exists(path):
-    sys.exit("File does not exist")
-if not path.split(".")[-1]=="py":
-    sys.exit("Not a Python file")
+target=sys.argv[2]
 
-with open(path, "r") as fh:
-    asu=fh.read()
-    print(len([i for i in asu.replace(" ", "").split("\n") if len(i)>0 and not i.startswith("#")]))
+try:
+    df=pd.read_csv(path)
+except:
+    sys.exit(f'Could not read {path}')
+
+pd.concat((pd.DataFrame([i.split(", ") for i in df["name"].to_numpy()], columns=["first", "last"]), df["house"]), axis=1).to_csv(target, index=False)
