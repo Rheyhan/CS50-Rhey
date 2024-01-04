ec3acdcf59553bc9fc943e8075a110aca1d4ac61
Rheyhan <rheyhanfahry@apps.ipb.ac.id>
2024-01-04 17:35:30 +0700

    Uwa uwa

---
+++ 8th week (re)/test_working.py
@@ -0,0 +1,16 @@
+from working import convert
+import pytest
+
+def test():
+    assert convert("9 AM to 5 PM")=="09:00 to 17:00"
+    assert convert("9:00 AM to 5:00 PM")=="09:00 to 17:00"
+    assert convert("10 PM to 8 AM")=="22:00 to 08:00"
+    assert convert("10:30 PM to 8:50 AM")=="22:30 to 08:50"
+
+def failtest():
+    with pytest.raises(ValueError):
+        convert("9:60 AM to 5:60 PM")
+    with pytest.raises(ValueError):
+        convert("9 AM - 5 PM")
+    with pytest.raises(ValueError):
+        convert("09:00 AM - 17:00 PM")
