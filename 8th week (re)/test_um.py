14ff91165542c6d8e228a88ed990cf2951686cbf
Rheyhan <rheyhanfahry@apps.ipb.ac.id>
2024-01-04 17:56:01 +0700

    Wong saya suka kok

---
+++ 8th week (re)/test_um.py
@@ -0,0 +1,7 @@
+from um import count
+
+def test():
+    assert count("um") == 1
+    assert count("um?") == 1
+    assert count("Um, thanks for the album.") == 1
+    assert count("Um, thanks, um...") == 2
--- 8th week (re)/test_working.py
+++ 8th week (re)/test_working.py
@@ -7,10 +7,15 @@ def test():
     assert convert("10 PM to 8 AM")=="22:00 to 08:00"
     assert convert("10:30 PM to 8:50 AM")=="22:30 to 08:50"
 
-def failtest():
+def test_wrong_format():
     with pytest.raises(ValueError):
-        convert("9:60 AM to 5:60 PM")
+        convert("9 AM - 9 PM")
+
+def test_wrong_minute():
     with pytest.raises(ValueError):
-        convert("9 AM - 5 PM")
+         convert("9:60 AM to 9:60 PM")
+
+def test_wrong_hour():
     with pytest.raises(ValueError):
-        convert("09:00 AM - 17:00 PM")
+        convert("13 PM to 17 PM")
+        
